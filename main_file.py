f64b1c53f83204e86038b72bd3d69deade5b2dda
ChyuanSenGuZe <Zkzcfg8520@gmail.com>
2021-12-21 00:53:52 +0800

    push

--- main_file.py
+++ main_file.py
@@ -10,5 +10,4 @@ root.geometry('600x800')
 lb1 = tk.Label(root, text = 'Hello World',bg = 'yellow' ,fg = '#263238', font = ('Arial', 12))
 lb1.grid(column = 0, row = 0)
 root.mainloop()
-##12313
-##7898
+##12
