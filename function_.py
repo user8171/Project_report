e0bb5993a3d44813ef78c484fd77a2e4d2f5cc4e
user8171 <arsf32303@mail.com>
2021-12-21 23:09:26 +0800

    push by user8171

--- function_.py
+++ function_.py
@@ -1,6 +1,7 @@
 #function here
 #-*- coding:utf-8 -*-
-from tkinter import EXCEPTION
+from tkinter import EXCEPTION, Label
+from tkinter.constants import NONE
 from typing import cast
 from requests.api import get
 from import_ import * 
@@ -15,14 +16,11 @@ if input == '關鍵字':
 #關鍵字對話程式
 #input_為使用者輸入文字, label_element為label元素
 def talking_function(input_, label_element):
-        chat_listcount = 0
+        #關鍵字清單
         interact_text = ['天氣','你好']
+        input_ = str(input_)
         if input_ == '天氣':
-                locate_area = ''
-                '''(這裡輸入子判斷式判斷要搜尋哪個區域),
-                        類似->input('請搜尋要查詢的行政區')
-                '''
-                #weather()#函式內放要查詢的行政區
+                weather(input_, label_element)
          #if input == '':
         else:
                 difflibfunction(input_, interact_text, label_element)
@@ -34,17 +32,19 @@ def difflibfunction(input_, interact_text, label_element):
                 data = difflib.get_close_matches(input_, interact_text, cutoff = 0.5, n=1)
                 label_element['text'] = '你是不是想說: ' + data[0]
         except:
-                label_element['text'] = '不支援的關鍵字。'
+                label_element['text'] = '不支援的關鍵字。' 
 
 #從這裡開始是定義對話關鍵字的函式
 #未來七天天氣預報
-def weather(input_location):
+def weather(input_location, label_element):
         collect_information = ['']
         url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314'
         locations = {'基隆市':'12', '臺北市':'9', '新北市':'3', '桃園市':'13', '新竹縣':'0', '新竹市':'21', '苗栗縣':'2', '臺中市':'20', '彰化縣':'8', '雲林縣':'5', '南投縣':'10', '嘉義縣':'18', '嘉義市':'17', '臺南市':'6', '高雄市':'7', '屏東縣':'19', '臺東縣':'16', '花蓮縣':'14', '宜蘭縣':'4', '金門縣':'1', '澎湖縣':'11', '連江縣':'15'}
         
         #判斷是否有資料
         try:
+                #label_element['text'] = '請輸入台灣行政區' 
+                print(type(input_location))
                 location_number = locations[input_location]
                 get_data = requests.get(url).text
                 get_data = json.loads(get_data)
@@ -57,24 +57,24 @@ def weather(input_location):
 
                 #凱擇,這裡讓使用者輸入「降雨機率預報」套用weather_predict_information(data)函式
                 #return型態為List,其內容為未來七天的天氣資訊,還有原網址
-                weather_predict_information(data)
+                weather_predict_information(data, label_element)
                 
 
         except KeyError:
-                #凱擇,這裡用Label修改文字="輸入未支援的地區"
                 #loction_list為可以被查詢的縣市
                 print("輸入未支援的地區")
                 locations_list = ['支援的縣市:']
                 for key in locations.keys():
                         locations_list[0] += key + ' '
                 print(locations_list[0])
+                label_element['text'] = '輸入未支援的地區。\n' + locations_list
 
         except Exception as e:
                 #凱擇,這裡用Label修改文字="發生未知錯誤"
-                print(f"發生未知錯誤,code: {e}")
+                label_element['text'] = '發生未知錯誤。錯誤代碼訊息: ' + e
 
 #未來七天降雨機率
-def weather_predict_information(data):
+def weather_predict_information(data, label_element):
         collect_weather_information = ['', '', '', '', '', '', '','']
         try:
                 data = data['weatherElement']
@@ -91,9 +91,4 @@ def weather_predict_information(data):
                 return collect_weather_information
         except Exception as e:
                 #凱擇,這裡用Label修改文字="發生未知錯誤"
-                print(f"發生未知錯誤,code: {e}")
-
-#test case
-#weather('新竹縣')
-#weather('基隆市')
-#weather('GG市')
+                label_element['text'] = '發生未知錯誤。錯誤代碼訊息: ' + e
