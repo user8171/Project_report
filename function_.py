220ecba282ecbc788f24a6ae9ddb83760b5af49e
user8171 <arsf32303@mail.com>
2021-12-29 22:22:55 +0800

    push by user8171

--- function_.py
+++ function_.py
@@ -2,64 +2,68 @@
 #-*- coding:utf-8 -*-
 from import_ import * 
 
-def btn_col(status_code, entry_element, label_element, button_element):
+def btn_col(status_code, entry_element, scrolledtext_element, button_element):
 	if entry_element.get() != '':
 		#status_code 設定為一般聊天
 		if status_code == 1:
-			talking_function(entry_element.get(), entry_element, label_element, button_element, status_code)
+			talking_function(entry_element.get(), entry_element, scrolledtext_element, button_element, status_code)
 			#entry_element.delete(0,"end")
 		#status_code 設定為查詢天氣資訊
 		elif status_code == 2:
-			weather(entry_element.get(), entry_element, label_element, button_element, status_code)
+			weather(entry_element.get(), entry_element, scrolledtext_element, button_element, status_code)
 			entry_element.delete(0,"end")
 
 #關鍵字對話程式
-#input_text為使用者輸入文字, label_element為label元素
-def talking_function(input_text, entry_element, label_element, button_element, status_code):
+#input_text為使用者輸入文字, scrolledtext_element為label元素
+def talking_function(input_text, entry_element, scrolledtext_element, button_element, status_code):
 	#關鍵字清單
 	interact_text = ['天氣','你好','時間','美食', '運動']
 	if input_text == '天氣':
-		weather(input_text, entry_element, label_element, button_element, status_code)
+		weather(input_text, entry_element, scrolledtext_element, button_element, status_code)
 		entry_element.delete(0,"end")
 	elif input_text == '你好':
-		hello(input_text, label_element)
+		hello(input_text, scrolledtext_element)
 		entry_element.delete(0,"end")
 	elif input_text == '時間':
-		get_time(input_text, label_element)
+		get_time(input_text, scrolledtext_element)
 		entry_element.delete(0,"end")
 	elif input_text == '美食':
-		good_food_osusume(input_text, label_element)
+		good_food_osusume(input_text, scrolledtext_element)
 		entry_element.delete(0,"end")
 	elif input_text == '運動':
-		exercise_osusume(input_text, label_element)
+		exercise_osusume(input_text, scrolledtext_element)
 		entry_element.delete(0,"end")
 	else:
-		difflibfunction(input_text, interact_text, label_element, entry_element)
+		difflibfunction(input_text, interact_text, scrolledtext_element, entry_element)
 	
 #模糊判斷
-def difflibfunction(input_text, interact_text, label_element, entry_element):
+def difflibfunction(input_text, interact_text, scrolledtext_element, entry_element):
 	#cutoff: 誤差, n:回傳幾筆近似資料
 	try:
 		data = difflib.get_close_matches(input_text, interact_text, cutoff = 0.5, n=1)
-		label_element['text'] = '你是不是想說: ' + data[0]
+		output_str = '你是不是想說: ' + data[0] + '\n'
+		scrolledtext_element.insert(tk.END, output_str)
 		entry_element.delete(0,"end")
 		entry_element.insert(0,data[0])
 	except:
-		label_element['text'] = '不支援的關鍵字。' 
+		output_str = '不支援的關鍵字。' + '\n' 
+		scrolledtext_element.insert(tk.END, output_str)
 
 #從這裡開始是定義對話關鍵字的函式
 #未來七天天氣預報 
 #def weather(input_text):
-def weather(input_text, entry_element, label_element, button_element, status_code):
+def weather(input_text, entry_element, scrolledtext_element, button_element, status_code):
 	if status_code == 1:
-		label_element['text'] = '請輸入台灣行政區,輸入「取消」以停止搜尋天氣資訊'
-		button_element['command'] = command=lambda: btn_col(2, entry_element, label_element, button_element)
+		output_str = '請輸入台灣行政區,輸入「取消」以停止搜尋天氣資訊' + '\n'
+		scrolledtext_element.insert(tk.END, output_str)
+		button_element['command'] = command=lambda: btn_col(2, entry_element, scrolledtext_element, button_element)
 		#button_element.configure(command=lambda: btn_col(2))
 		return True
 	elif input_text == '取消':
 		#button_element['command'] = 'command=lambda: btn_col(1)' 
-		button_element.configure(command=lambda: btn_col(1, entry_element, label_element, button_element))
-		label_element['text'] = '已結束搜尋天氣資訊。' 
+		button_element.configure(command=lambda: btn_col(1, entry_element, scrolledtext_element, button_element))
+		output_str = '已結束搜尋天氣資訊。'  + '\n'
+		scrolledtext_element.insert(tk.END, output_str)
 		return 0
         
 	#修改button的status_code
@@ -80,22 +84,24 @@ def weather(input_text, entry_element, label_element, button_element, status_cod
 		data = data[int(location_number)]
 
 		#return型態為List,其內容為未來七天的天氣資訊,還有原網址
-		weather_predict_information(data, label_element)
+		weather_predict_information(data, scrolledtext_element)
 		
 	except KeyError:
 		#loction_list為可以被查詢的縣市
 		locations_list = ['支援的縣市:']
 		for key in locations.keys():
 			locations_list[0] += key + ' '
-		label_element['text'] = '輸入未支援的地區。\n\n' + locations_list[0] +'\n請重新輸入。'
-		button_element.configure(command=lambda: btn_col(2, entry_element, label_element, button_element))
+		output_str = '輸入未支援的地區。\n\n' + locations_list[0] +'\n請重新輸入。' + '\n'
+		scrolledtext_element.insert(tk.END, output_str)
+		button_element.configure(command=lambda: btn_col(2, entry_element, scrolledtext_element, button_element))
 		pass
 
 	except Exception as e:
-		label_element['text'] = '發生未知錯誤。錯誤代碼訊息: ' + e
-        
+		output_str = '發生未知錯誤。錯誤代碼訊息: ' + e + '\n'
+		scrolledtext_element.insert(tk.END, output_str)
+
 #未來七天降雨機率
-def weather_predict_information(data, label_element):
+def weather_predict_information(data, scrolledtext_element):
 	collect_weather_information = ''
 	try:
 		data = data['weatherElement']
@@ -109,24 +115,28 @@ def weather_predict_information(data, label_element):
 			present_data = present_data[0]
 			collect_weather_information += present_data.get('value') + '\n\n'
 		collect_weather_information += "更多資訊: https://data.gov.tw/dataset/9308" + '\n'
-		label_element['text'] = collect_weather_information
-		return collect_weather_information
+		output_str = collect_weather_information
+		scrolledtext_element.insert(tk.END, output_str)
+
 	except Exception as e:
-		label_element['text'] = '發生未知錯誤。錯誤代碼訊息: ' + e
+		output_str = '發生未知錯誤。錯誤代碼訊息: ' + e
+		scrolledtext_element.insert(tk.END, output_str)
 
-def hello(input_, label_element):
-	label_element['text'] = '您好,歡迎與我聊天'
+def hello(input_, scrolledtext_element):
+	output_str = '您好,歡迎與我聊天' + '\n'
+	scrolledtext_element.insert(tk.END, output_str)
 
-def get_time(input_, label_element):
-	label_element['text'] = '現在時間是: ' + str(datetime.datetime.now())
+def get_time(input_, scrolledtext_element):
+	output_str = '現在時間是: ' + str(datetime.datetime.now()) + '\n'
+	scrolledtext_element.insert(tk.END, output_str)
 
-def good_food_osusume(input_, label_element):
-	good_food_list = ['藍獸亓--香酥黃金雞飯',  '二口炒飯--綜合鐵板麵', '快炒王--火腿蛋炒飯', '福禾--無骨雞丁便當', '古早味便當--大雞腿便當', '翔鶴--雞排便當', '樂謎雞排--原味雞排', '八兩--這三小', '豪大要倒了']
+def good_food_osusume(input_, scrolledtext_element):
+	good_food_list = ['藍獸亓--香酥黃金雞飯',  '二口炒飯--綜合鐵板麵', '快炒王--火腿蛋炒飯', '福禾--無骨雞丁便當', '古早味便當--大雞腿便當', '翔鶴--雞排便當', '樂謎雞排--原味雞排', '八兩--雞排', '豪大要倒了']
 	good_drink_list = ['水云茶堂--珍珠紅茶無糖去冰', '多多茶坊--核廢料', '喫茶小舖--薑汁撞奶', '春水堂--珍珠奶茶']
-	#output_str =good_drink_list[random.randint(0,len(good_drink_list)-1)]
-	#print(type(output_str))
-	label_element['text'] = '今天推薦 ' + good_food_list[random.randint(0, len(good_food_list)-1)] + ',搭配 ' + good_drink_list[random.randint(0, len(good_drink_list)-1)]
+	output_str = '今天推薦 ' + good_food_list[random.randint(0, len(good_food_list)-1)] + ',搭配 ' + good_drink_list[random.randint(0, len(good_drink_list)-1)] + '\n'
+	scrolledtext_element.insert(tk.END, output_str)
 
-def exercise_osusume(input_, label_element):
+def exercise_osusume(input_, scrolledtext_element):
 	exercise_list = ['籃球', '羽球', '網球', '排球']
-	label_element['text'] = '運動建議: ' + exercise_list[random.randint(0, len(exercise_list))-1]
+	output_str = '運動建議: ' + exercise_list[random.randint(0, len(exercise_list))-1] + '\n'
+	scrolledtext_element.insert(tk.END, output_str)
