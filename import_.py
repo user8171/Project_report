e69614e98ff4cf3342e87ef26047d8eb28bf5ebf
user8171 <arsf32303@mail.com>
2021-12-30 13:55:33 +0800

    push by user8171

--- import_.py
+++ import_.py
@@ -4,8 +4,7 @@ import tkinter as tk
 import urllib.request as ur
 import json
 
-from tkinter import EXCEPTION, Label
+from tkinter import EXCEPTION, Label, scrolledtext
 from tkinter.constants import NONE
 from requests.api import get
 from function_ import *
-from tkinter import scrolledtext
